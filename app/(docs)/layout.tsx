14c09b921f2fcb76cbf808c80d1702a2e05e1a81
cal <po6271933@outlook.com>
2022-12-30 23:04:34 +0800

    fix

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -9,7 +9,7 @@ interface DocsLayoutProps {
 export default function DocsLayout({children}: DocsLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="px-4 sticky top-0 z-40 w-full border-b border-slate-200 bg-docsy-blue" p>
+      <header className="px-4 sticky top-0 z-40 w-full border-b border-slate-200 bg-docsy-blue">
         <div className="flex items-center h-16 sm:justify-between">
           <MainNav items={ MainNavRoute }/>
         </div>
